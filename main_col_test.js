c13433683941bd8e866a679ef25d6af7a7c54b0c
nevraw <nevraw@users.noreply.github.com>
2015-08-08 10:58:18 +0100

    Create main_col_test.js

---
+++ main_col_test.js
@@ -0,0 +1,112 @@
+(function() {
+ loadOptions();
+ buttonHandler();
+})();
+
+function buttonHandler() {
+ var $submitButton = $('#submitButton');
+
+ $submitButton.on('click', function() {
+  console.log('Submit');
+ 
+  var return_to = getQueryParam('return_to', 'pebblejs://close#');
+  document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
+ });
+
+ var $cancelButton = $('#cancelButton');
+
+ $cancelButton.on('click', function() {
+  console.log('Cancel');
+ 
+  var return_to = getQueryParam('return_to', 'pebblejs://close#');
+  document.location = return_to;
+ });
+}
+
+function loadOptions() {
+ var $hourColorPicker = $('#hourColorPicker');
+ var $min5ColorPicker = $('#min5ColorPicker');
+ var $minColorPicker = $('#minColorPicker');
+ var $preset = $('#preset');
+
+ if (localStorage.hourColor) {
+  $hourColorPicker[0].value = localStorage.hourColor;
+ }
+ if (localStorage.min5Color) {
+  $min5ColorPicker[0].value = localStorage.min5Color;
+ }
+ if (localStorage.minColor) {
+  $minColorPicker[0].value = localStorage.minColor;
+ }
+ if (localStorage.preset) {
+  $preset = localStorage.preset;
+ }
+} 
+
+var x = 0;
+function hola(x) {
+ if(x == 0) {
+  document.getElementById("cont1").style.visibility="hidden";
+  document.getElementById("cont1").style.display="none";
+  document.getElementById("cont2").style.visibility="hidden";
+  document.getElementById("cont2").style.display="none";
+ }
+ if(x == 1) {
+  document.getElementById("cont1").style.visibility="visible";
+  document.getElementById("cont1").style.display="block";
+  document.getElementById("cont2").style.visibility="hidden"; 
+  document.getElementById("cont2").style.display="none";
+ }
+ if(x == 2)  {
+  document.getElementById("cont1").style.visibility="hidden";
+  document.getElementById("cont1").style.display="none";
+  document.getElementById("cont2").style.visibility="visible"; 
+  document.getElementById("cont2").style.display="block";
+ }
+}
+function hideBoth()  
+{  
+  document.getElementById("cont1").style.visibility="hidden";
+  document.getElementById("cont1").style.display="none";
+  document.getElementById("cont2").style.visibility="hidden";
+  document.getElementById("cont2").style.display="none";
+}
+
+
+function getAndStoreConfigData() {
+ var $hourColorPicker = $('#hourColorPicker');
+ var $min5ColorPicker = $('#min5ColorPicker');
+ var $minColorPicker = $('#minColorPicker');
+ var $preset = $('#preset');
+
+ if ($preset != 1 && $preset != 2) {
+  $preset = 0;
+ }
+
+ var options = {
+  hourColor: $hourColorPicker.val(),
+  min5Color: $min5ColorPicker.val(),
+  minColor: $minColorPicker.val(),
+  preset:  $preset;
+ };
+ 
+ localStorage.hourColor = options.hourColor;
+ localStorage.min5Color = options.min5Color;
+ localStorage.minColor = options.minColor;
+ localStorage.preset = options.preset;
+
+ console.log('Got options: ' + JSON.stringify(options));
+ return options;
+}
+
+function getQueryParam(variable, defaultValue) {
+ var query = location.search.substring(1);
+ var vars = query.split('&');
+ for (var i = 0; i < vars.length; i++) {
+  var pair = vars[i].split('=');
+  if (pair[0] === variable) {
+   return decodeURIComponent(pair[1]);
+  }
+ }
+ return defaultValue || false;
+}
